9a27022c6443a5e18de4cc734a40a2ac2b5f53dd
hasegawa kazuya <hasegawakazuya@hasegawa-no-MacBook-Air.local>
2015-07-03 02:51:32 +0900

    youtube,new,change,save

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,6 +14,18 @@ import static play.data.Form.form;
 import play.filters.csrf.AddCSRFToken;
 import play.filters.csrf.RequireCSRFCheck;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import org.w3c.dom.*;
+
 
 public class Application extends Controller {
 	public static Result index() {
@@ -31,7 +43,9 @@ public class Application extends Controller {
 	public static Result edit_page() {
 		return ok(edit_page.render("edit"));
 	}
-
+    public static Result edit_head() {
+        return ok(edit_head.render());
+    }
 
 	
 	
@@ -110,5 +124,142 @@ public class Application extends Controller {
 			return redirect(routes.Application.index());
 		}
 	}
-	
+    /**
+     * ページ新規作成アクション
+     */
+    public static Result dicition_name(){
+        return ok(edit_new_page.render(""));
+    }
+    
+    public static Result new_page(){
+        String name = Form.form().bindFromRequest().get("name");
+        System.out.println(name);
+        create_page(name);
+        return redirect("/edit_page");
+    }
+    
+    public static void create_page(String name){
+        String dir = System.getProperty("user.dir");
+        System.out.println(dir);
+        File file = new File(dir + "/user/" + name + ".html");
+        PrintWriter pw = null;
+        try {
+            // 出力ストリームを生成します。
+            pw = new PrintWriter(
+                                 new OutputStreamWriter(
+                                                        new FileOutputStream(file)));
+            // ここでファイルに文字を書き込みます。
+            pw.println("<!DOCTYPE html>");
+            pw.println("<html>");
+            pw.println("<head><title>新規作成</title></head>");
+            pw.println("<body>");
+            pw.println("<h1>新規ページ</h1>");
+            pw.println("<p>これはcreate_pageにて作成されました。</p>");
+            pw.println("</body>");
+            pw.println("</html>");
+            
+            System.out.println("ファイルの作成に成功しました!"); //作成後に、特定のファイルを呼び出す場合の処理が未実装
+            //制作時は、サーバから表示するページのデータを取得し、1行ずつ読む工程で描画する必要性あり。
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            // クローズ処理（成功・失敗に関わらず必ずクローズします。）
+            // クローズ漏れはバグのもとになります。必ずfinally句でクローズしましょう。
+            if (pw != null) {
+                pw.close();
+            }
+        }
+    }
+    /**
+     * ディレクトリ作成アクション (現状は動いていません。)
+     */
+    /*
+     File newdir = new File("c:¥¥tmp¥¥sub");
+     newdir.mkdir();
+     */
+    
+    /**
+     * 保存アクション
+     */
+    public static Result save(){
+        String dir = System.getProperty("user.dir");
+        File file = new File(dir + "/user/" + "test.html");  //どのファイルを変更するかの定義が未実装
+        PrintWriter pw = null;
+        try {
+            // 出力ストリームを生成します。
+            pw = new PrintWriter(
+                                 new OutputStreamWriter(
+                                                        new FileOutputStream(file)));
+            
+            
+            // ここでファイルに文字を書き込みます。
+            pw.println("stringその1");
+            pw.println("stringその2");
+            
+            
+            System.out.println("ファイルの書き込みに成功しました!");
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            // クローズ処理（成功・失敗に関わらず必ずクローズします。）
+            // クローズ漏れはバグのもとになります。必ずfinally句でクローズしましょう。
+            if (pw != null) {
+                pw.close();
+            }
+        }
+        return redirect("/edit_head");
+    }
+    
+    /**
+     * 構文解析アクション
+     */
+    /*
+     public static Result analysis() throws SAXException, IOException{
+     // DOMを解釈するパーサを準備
+     DOMParser parser = new DOMParser();
+     parser.setFeature("http://xml.org/sax/features/namespaces", false);
+     
+     // 引数があれば第１引数をurlStrにセット、なければ"test.html"をセット
+     // String urlStr = args.length>0 ? args[0] : "test.html";
+     String urlStr =  "/edit_page";
+     System.out.println("SOURCE URL: " + urlStr); //urlStrを表示
+     
+     // 出力ファイル名を生成
+     //$は一番最後のという正規表現
+     String dir = System.getProperty("user.dir");
+     System.out.println(dir);
+     File file = new File(dir + "/test.txt");
+     //String resultFileName = urlStr.replaceAll("../data", "../result").replaceAll("html$", "image_and_text");
+     String resultFileName = dir + "/test.txt";
+     System.out.println(resultFileName);
+     
+     // 出力ファイルをオープン
+     FileOutputStream fos = new FileOutputStream(resultFileName);
+     OutputStreamWriter osw = new OutputStreamWriter(fos , "UTF-8");
+     BufferedWriter bw = new BufferedWriter(osw);
+     
+     // urlStrのファイルをパーサで読み込み、parserにDOMツリーを生成
+     parser.parse(urlStr);
+     // parser内のDOMツリーのドキュメントノードをdocumentにセットする
+     Document document =parser.getDocument();
+     // 指定したタグ名にもつ要素を全て取得する
+     NodeList nodeList = document.getElementsByTagName("title");
+     // 各要素を出力ファイルに出力する
+     for(int i=0; i < nodeList.getLength(); i++){
+     Element element = (Element)nodeList.item(i);
+     System.out.println(element.getTextContent());
+     
+     // outTextを出力ファイルに出力
+     // bw.write(outText);
+     }
+     
+     // 出力が完了したら出力ファイルを閉じる
+     bw.close();
+     osw.close();
+     fos.close();
+     return redirect("/edit_menu");
+     
+     }
+     */
+
 }
